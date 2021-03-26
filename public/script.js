4b40572776ba1fe2f7625825372eebb2d16cdb0a
Kati Ilieva <78005557+ks-ilieva@users.noreply.github.com>
2021-03-26 16:00:28 -0400

    EDITS html and js

---
+++ public/script.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-console */
+
+async function windowActions() {
+  console.log('window loaded');
+  const request = await fetch('/api/dining/');
+  const data = await request.json();
+  console.log(data);
+
+  const table = document.querySelector('.table');
+  const headerRow = document.querySelector('.table-row');
+
+  data.forEach((element) => {
+    const cellHead = document.createElement('th');
+    cellHead.classList.add('table-cell-heading');
+    cellHead.innerText = element;
+    headerRow.append(cellHead);
+
+    const tableRow = document.createElement('tr');
+    const diningId = document.createElement('td');
+    const diningName = document.createElement('td');
+    const diningAddress = document.createElement('td');
+
+    diningId.innerText = element.hall_id;
+    diningName.innerText = element.hall_name;
+    diningAddress.innerText = element.hall_address;
+
+    table.append(tableRow);
+    tableRow.append(diningId);
+    tableRow.append(diningName);
+    tableRow.append(diningAddress);
+  });
+}
+
+window.onload = windowActions();
+
+/*
+async function fetchDining() {
+  const response = await fetch('/api/dining/');
+  const diningHall = await response.json();
+  return diningHall;
+}
+
+// create table
+async function createTable(tableData) {
+  const diningData = tableData.data;
+  diningData.forEach((element) => {
+    const tableRow = document.createElement('tr');
+    const tableHeadId = document.createElement('th'); // for each element
+    const diningId = document.createTextNode(diningData.hall_id);
+    tableHeadId.appendChild(diningId);
+    tableRow.appendChild(tableHeadId.diningId);
+  });
+}
+
+async function windowActions() {
+  const myData = await fetchDining();
+  createTable(myData);
+}
+*/
